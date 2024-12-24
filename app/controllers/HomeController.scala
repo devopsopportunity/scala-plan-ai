78e4e95a7d05803f051d54061ffd1291337f7c21
Devops Opportunity <151925700+devopsopportunity@users.noreply.github.com>
2024-12-24 17:39:01 +0100

    Added log viewers for past and future logs.  - Implemented functionality to display logs from the "log-passati" directory. - Introduced a feature to display future activities in a separate section. - Enhanced user experience by showing appropriate messages when no logs are available. - Cleaned up the views to make them more user-friendly and informative. - Added navigation links for easy access to different log sections.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,6 +4,10 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
+import java.io.File  // Importa la classe File per lavorare con i file
+import java.io.IOException
+import scala.io.Source  // Importa Source per leggere i file
+
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
@@ -18,34 +22,91 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
+ // Metodo per la home page
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
 
-  /* 
-   * primo metodo in scala! wow chat! sono emozionato!!! 
-   * ma che roba e'? un incrocio Java-Python? ahhahahahah
-   * ci divertiremo tantissimo!!!!!!!! ma che figata!!!!!!
-   * ******************************************************
-  */
+  // Endpoint About
   def about() = Action {
     Ok(views.html.about())
   }
-  
- def menu() = Action { implicit request =>
+
+  // Endpoint per il menu
+  def menu() = Action { implicit request =>
     Ok(views.html.index())
-}
+  }
 
-def logPassati() = Action { implicit request =>
-    Ok(views.html.logPassati())
-}
+ def visualizzaLog(nomeFile: String) = Action { implicit request =>
+  val referer = request.headers.get("Referer").getOrElse(routes.HomeController.menu().url)
+  val baseUrl = routes.HomeController.menu().absoluteURL()
+  
+  // Estrai la parte relativa dalla Referer rimuovendo la base URL
+  val relativePath = referer.stripPrefix(baseUrl)
+  
+  // Ottieni solo l'ultima parte del path (es. "log-passati")
+  val lastSegment = relativePath.split("/").lastOption.getOrElse("menu")
 
-def logCorrente() = Action { implicit request =>
+  println(s"Ultimo segmento: $lastSegment")
+
+  val projectRootPath = System.getProperty("user.dir")
+  val logDirectoryPath = s"$projectRootPath/public/mylogs/"
+  val filePath = logDirectoryPath + "/" + lastSegment + "/" + nomeFile
+  
+   if (new java.io.File(filePath).exists()) {
+     val fileContent = scala.io.Source.fromFile(filePath).getLines().mkString("\n")
+     Ok(views.html.visualizzaLog(fileContent, referer))
+   } else {
+    Ok(views.html.visualizzaLog(s"Errore: Il file '$nomeFile' non esiste.", referer))
+   }
+  }
+
+  // Funzione per mostrare i log correnti
+  def logCorrente() = Action { implicit request =>
     Ok(views.html.logCorrente())
+  }
+
+// Funzione generica per mostrare i log
+def mostraLog(tipo: String) = Action { implicit request =>
+    val projectRootPath = System.getProperty("user.dir")
+    val logDirectoryPath = s"$projectRootPath/public/mylogs/$tipo"
+    
+    val folder = new File(logDirectoryPath)
+    
+    if (!folder.exists()) {
+        folder.mkdirs()
+        val message = s"Cartella '$logDirectoryPath' creata con successo."
+        tipo match {
+            case "log-passati" => Ok(views.html.logPassati(message, List())) // Log passati
+            case "log-futuri" => Ok(views.html.logFuturi(message, List()))  // Log futuri
+        }
+    } else {
+        var message = s"La cartella '$logDirectoryPath' contiene: "
+        var fileList = List[String]() // Lista per i file trovati
+        
+        if (folder.isDirectory) {
+            val logFiles = folder.listFiles().filter(_.getName.endsWith(".yaml"))
+            if (logFiles.nonEmpty) {
+                fileList = logFiles.map(_.getName).toList
+                message += s"File trovati: ${fileList.mkString(", ")}."
+            } else {
+                message += "Nessun file YAML trovato."
+            }
+        } else {
+            message += s"Errore inatteso su cartella '$logDirectoryPath'."
+        }
+        
+        tipo match {
+            case "log-passati" => Ok(views.html.logPassati(message, fileList)) // Log passati
+            case "log-futuri" => Ok(views.html.logFuturi(message, fileList))  // Log futuri
+        }
+    }
 }
 
-def logFuturi() = Action { implicit request =>
-    Ok(views.html.logFuturi())
-}
+  // Funzione per log passati
+  def logPassati() = mostraLog("log-passati")
+
+  // Funzione per log futuri
+  def logFuturi() = mostraLog("log-futuri")
 
 }
